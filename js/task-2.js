204923692ac4f1189eae69e2f5857c2355277dbc
Sergii Taran <serg.taran1970@gmail.com>
2025-11-28 19:53:33 +0200

    add solution task 2.1 from module 02

--- js/task-2.js
+++ js/task-2.js
@@ -1,89 +1,112 @@
 'use strict';
 
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
+// function makeTransaction(quantity, pricePerDroid, customerCredits) {
+//   const totalPrice = quantity * pricePerDroid;
 
-  if (totalPrice > customerCredits) {
+//   if (totalPrice > customerCredits) {
+//     return 'Insufficient funds!';
+//   } else {
+//     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+//   }
+// }
+
+// console.log(makeTransaction(5, 3000, 23000));
+// console.log(makeTransaction(3, 1000, 15000));
+// console.log(makeTransaction(10, 5000, 8000));
+// console.log(makeTransaction(8, 2000, 10000));
+// console.log(makeTransaction(10, 500, 5000));
+
+// ===================== //
+
+// function formatMessage(message, maxLength) {
+//   if (message.length <= maxLength) {
+//     return message;
+//   } else {
+//     return message.slice(0, maxLength) + '...';
+//   }
+// }
+
+// console.log(formatMessage('Curabitur ligula sapien', 16));
+// console.log(formatMessage('Curabitur ligula sapien', 23));
+// console.log(formatMessage('Vestibulum facilisis purus nec', 20));
+// console.log(formatMessage('Vestibulum facilisis purus nec', 30));
+// console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15));
+// console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41));
+
+// ===================== //
+
+// function checkForSpam(message) {
+//   const lowerCaseMessage = message.toLowerCase();
+
+//   if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(checkForSpam('Latest technology news'));
+// console.log(checkForSpam('JavaScript weekly newsletter'));
+// console.log(checkForSpam('Get best sale offers now!'));
+// console.log(checkForSpam('Amazing SalE, only tonight!'));
+// console.log(checkForSpam('Trust me, this is not a spam message'));
+// console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
+// console.log(checkForSpam('[SPAM] How to earn fast money?'));
+
+// ===================== //
+
+// function getShippingCost(country) {
+//   let price;
+
+//   switch (country) {
+//     case 'China':
+//       price = 100;
+//       break;
+//     case 'Chile':
+//       price = 250;
+//       break;
+//     case 'Australia':
+//       price = 170;
+//       break;
+//     case 'Jamaica':
+//       price = 120;
+//       break;
+//     case 'Ukraine':
+//       price = 300;
+//       break;
+
+//     default:
+//       return 'Sorry, there is no delivery to your country';
+//   }
+//   return `Shipping to ${country} will cost ${price} credits`;
+// }
+
+// console.log(getShippingCost('Australia'));
+// console.log(getShippingCost('Germany'));
+// console.log(getShippingCost('China'));
+// console.log(getShippingCost('Chile'));
+// console.log(getShippingCost('Jamaica'));
+// console.log(getShippingCost('Sweden'));
+
+// ===================== //
+
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const totalDroidsCost = quantity * pricePerDroid;
+
+  if (totalDroidsCost > customerCredits) {
     return 'Insufficient funds!';
   } else {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    return `You ordered ${quantity} droids worth ${totalDroidsCost} credits!`;
   }
 }
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
 
-// ------------------
-
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
-  } else {
-    return message.slice(0, maxLength) + '...';
-  }
-}
-
-console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
-console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
-console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
-console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
-
-// ------------------
-
-function checkForSpam(message) {
-  const lowerCaseMessage = message.toLowerCase();
-
-  if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
-
-// -----------------
-
-function getShippingCost(country) {
-  let price;
-
-  switch (country) {
-    case 'China':
-      price = 100;
-      break;
-    case 'Chile':
-      price = 250;
-      break;
-    case 'Australia':
-      price = 170;
-      break;
-    case 'Jamaica':
-      price = 120;
-      break;
-    case 'Ukraine':
-      price = 300;
-      break;
-
-    default:
-      return 'Sorry, there is no delivery to your country';
-  }
-  return `Shipping to ${country} will cost ${price} credits`;
-}
-
-console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingCost('Germany')); // "Sorry, there is no delivery to your country"
-console.log(getShippingCost('China')); // "Shipping to China will cost 100 credits"
-console.log(getShippingCost('Chile')); // "Shipping to Chile will cost 250 credits"
-console.log(getShippingCost('Jamaica')); // "Shipping to Jamaica will cost 120 credits"
-console.log(getShippingCost('Sweden')); // "Sorry, there is no delivery to your country"
+// ===================== //
+// ===================== //
+// ===================== //
+// ===================== //
