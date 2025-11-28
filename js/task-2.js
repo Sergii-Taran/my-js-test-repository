c7ef05b6f9297e796512cb125964866640f9b638
Sergii Taran <serg.taran1970@gmail.com>
2025-11-28 20:22:27 +0200

    add solution task 2.2 from module 02

--- js/task-2.js
+++ js/task-2.js
@@ -107,6 +107,22 @@ console.log(makeTransaction(8, 2000, 10000));
 console.log(makeTransaction(10, 500, 5000));
 
 // ===================== //
+
+function formatMessage(message, maxLength) {
+  if (message.length <= maxLength) {
+    return message;
+  } else {
+    return message.slice(0, maxLength) + '...';
+  }
+}
+
+console.log(formatMessage('Curabitur ligula sapien', 16));
+console.log(formatMessage('Curabitur ligula sapien', 23));
+console.log(formatMessage('Vestibulum facilisis purus nec', 20));
+console.log(formatMessage('Vestibulum facilisis purus nec', 30));
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15));
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41));
+
 // ===================== //
 // ===================== //
 // ===================== //
