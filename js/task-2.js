459790d64aa73f396552e24be38303288c18f709
Sergii Taran <serg.taran1970@gmail.com>
2025-11-28 21:21:25 +0200

    add solution task 2.4 from module 02

--- js/task-2.js
+++ js/task-2.js
@@ -140,4 +140,35 @@ console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
 console.log(checkForSpam('[SPAM] How to earn fast money?'));
 
 // ===================== //
+
+function getShippingCost(country) {
+  let price;
+
+  switch (country) {
+    case 'China':
+      price = 100;
+      break;
+    case 'Chile':
+      price = 250;
+      break;
+    case 'Australia':
+      price = 170;
+      break;
+    case 'Jamaica':
+      price = 120;
+      break;
+
+    default:
+      return 'Sorry, there is no delivery to your country';
+  }
+  return `Shipping to ${country} will cost ${price} credits`;
+}
+
+console.log(getShippingCost('Australia'));
+console.log(getShippingCost('Germany'));
+console.log(getShippingCost('China'));
+console.log(getShippingCost('Chile'));
+console.log(getShippingCost('Jamaica'));
+console.log(getShippingCost('Sweden'));
+
 // ===================== //
