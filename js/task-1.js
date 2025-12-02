783febeba362964f6a00726c8f47298c06db511f
Sergii Taran <serg.taran1970@gmail.com>
2025-12-02 10:22:56 +0200

    add solution task 02 from module 01

--- js/task-1.js
+++ js/task-1.js
@@ -6,22 +6,40 @@
 // totalPrice - загальна сума замовлення до сплати (ціна одного дроїда помножена на кількість дроїдів у замовленні, плюс вартість доставки)
 // message - повідомлення про стан замовлення у форматі "You ordered droids worth <total price> credits"
 
-const pricePerDroid = 800;
-const orderedQuantity = 6;
-const deliveryFee = 50;
-const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
-const message = `You ordered ${orderedQuantity} droids worth ${totalPrice} credits`;
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered ${orderedQuantity} droids worth ${totalPrice} credits`;
 
-console.log(totalPrice);
+// console.log(totalPrice);
+// console.log(message);
+
+// ======================= //
+// ======= task-02 ======= //
+
+// Змінна username зберігає рядок з іменем користувача.
+// Оголоси змінну message та за допомогою синтаксису шаблонних рядків. Доповни код так, щоб в ній зберігався рядок наступного формату: "Username <name> is <length> characters long", в якому <name> це ім'я користувача зі змінної username, а <length> це його довжина.
+
+const userName = 'Sergio';
+const message = `Username ${userName} is ${userName.length} characters long`;
+
 console.log(message);
 
 // ======================= //
-
 // ======= task-01 ======= //
-// ======================= //
 
-// ======= task-01 ======= //
 // ======================= //
+// ======= task-01 ======= //
 
-// ======= task-01 ======= //
 // ======================= //
+// ======= task-01 ======= //
+
+// ======================= //
+// ======= task-01 ======= //
+
+// ======================= //
+// ======= task-01 ======= //
+
+// ======================= //
+// ======= task-01 ======= //
