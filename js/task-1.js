b9d868d388de7c81afefd979340e8948c489aa74
Sergii Taran <serg.taran1970@gmail.com>
2025-12-03 13:25:57 +0200

    add solution hw 03 from module 01

--- js/task-1.js
+++ js/task-1.js
@@ -128,3 +128,23 @@ console.log(getShippingMessage('Sweden', 100, 20));
 
 // =========================== //
 // ======= homework-03 ======= //
+// Оголоси функцію getElementWidth, яка очікує три параметри, значення яких будуть задаватися під час її виклику:
+// • content — перший параметр, ширина контенту
+// • padding — другий параметр, значення горизонтального падінгу для кожної зі сторін
+// • border — третій параметр, значення товщини бордера для кожної зі сторін
+// Значення всіх параметрів будуть рядками формату Npx де N — це довільне число, ціле або дробове
+// Доповни код функції так, щоб вона повертала число —загальну ширину елемента
+// При розрахунку загальної ширини орієнтуйся на те, що значення box-sizing дорівнює border-box
+
+function getElementWidth(content, padding, border) {
+  return (
+    Number.parseFloat(content) +
+    2 * Number.parseFloat(padding) +
+    2 * Number.parseFloat(border)
+  );
+}
+
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
+// =========================== //
