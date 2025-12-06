f0f2a8c8662a60afeb44e9bb39cf29e07f891228
Sergii Taran <serg.taran1970@gmail.com>
2025-12-06 08:58:16 +0200

    add solution hw-03 from module 02

--- js/task-2.js
+++ js/task-2.js
@@ -411,23 +411,46 @@
 // Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий рядок без змін.
 // Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці та повертає обрізану версію.
 
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
-  } else {
-    return message.slice(0, maxLength) + '...';
-  }
-}
+// function formatMessage(message, maxLength) {
+//   if (message.length <= maxLength) {
+//     return message;
+//   } else {
+//     return message.slice(0, maxLength) + '...';
+//   }
+// }
 
-console.log(formatMessage('Curabitur ligula sapien', 16));
-console.log(formatMessage('Curabitur ligula sapien', 23));
-console.log(formatMessage('Vestibulum facilisis purus nec', 20));
-console.log(formatMessage('Vestibulum facilisis purus nec', 30));
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15));
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41));
+// console.log(formatMessage('Curabitur ligula sapien', 16));
+// console.log(formatMessage('Curabitur ligula sapien', 23));
+// console.log(formatMessage('Vestibulum facilisis purus nec', 20));
+// console.log(formatMessage('Vestibulum facilisis purus nec', 30));
+// console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15));
+// console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41));
 
 // =========================== //
 // ======= homework-03 ======= //
+
+// Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки
+// Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
+// Доповни код функції таким чином, що:
+// Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
+// Якщо в рядку відсутні заборонені слова, функція повертає буль false
+
+function checkForSpam(message) {
+  const normalize = message.toLowerCase();
+
+  return normalize.includes('spam') || normalize.includes('sale')
+    ? true
+    : false;
+}
+
+console.log(checkForSpam('Latest technology news'));
+console.log(checkForSpam('JavaScript weekly newsletter'));
+console.log(checkForSpam('Get best sale offers now!'));
+console.log(checkForSpam('Amazing SalE, only tonight!'));
+console.log(checkForSpam('Trust me, this is not a spam message'));
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
+console.log(checkForSpam('[SPAM] How to earn fast money?'));
+
 // =========================== //
 // ======= homework-04 ======= //
 // =========================== //
