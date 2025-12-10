d6e5b3c384388c33571ca0c3426480c642bff9cd
Sergii Taran <serg.taran1970@gmail.com>
2025-12-10 22:31:12 +0200

    add solution homework-03 from module 03

--- js/task-3.js
+++ js/task-3.js
@@ -307,23 +307,48 @@
 // Якщо кількість елементів у новому масиві перевищує maxLength, функція повинна повернути копію масиву з довжиною maxLength елементів
 // В іншому випадку функція повинна повернути весь новий масив
 
-function makeArray(firstArray, secondArray, maxLength) {
-  const newArray = firstArray.concat(secondArray);
+// function makeArray(firstArray, secondArray, maxLength) {
+//   const newArray = firstArray.concat(secondArray);
 
-  if (newArray.length <= maxLength) {
-    return newArray;
-  }
-  return newArray.slice(0, maxLength);
-}
+//   if (newArray.length <= maxLength) {
+//     return newArray;
+//   }
+//   return newArray.slice(0, maxLength);
+// }
 
-console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3));
-console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4));
-console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3));
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2));
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4));
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0));
+// console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3));
+// console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4));
+// console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3));
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2));
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4));
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0));
+
 // ===================== //
 // ======= hw-03 ======= //
-// ======================= //
-// ======= task-18 ======= //
+
+// Напиши функцію filterArray(numbers, value), яка приймає масив чисел (numbers) та значення (value) як параметри
+// Функція повинна повертати новий масив лише тих чисел із масиву numbers, які більші за значення value
+// Усередині функції:
+// Створи порожній масив, у який будеш додавати підходящі числа.
+// Використай цикл для ітерації кожного елемента масиву numbers.
+// Використовуй умовний оператор if усередині циклу для перевірки кожного елемента и додавання до свого масиву.
+// Поверни свій новий масив з підходящими числами як результат.
+
+function filterArray(numbers, value) {
+  const result = [];
+
+  for (const num of numbers) {
+    if (num > value) {
+      result.push(num);
+    }
+  }
+  return result;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
+
 // ======================= //
