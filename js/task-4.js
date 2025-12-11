a46c751530922fd63626d877b98c0726f6be264b
Sergii Taran <serg.taran1970@gmail.com>
2025-12-11 11:13:38 +0200

    add solution task-04 from module 04

--- js/task-4.js
+++ js/task-4.js
@@ -69,24 +69,47 @@
 // Перебери об'єкт apartment, використовуючи метод Object.keys() і цикл for...of
 // Запиши у змінну keys масив ключів властивостей об'єкта apartment, і додай в масив values всі значення його властивостей
 
-const apartment = {
-  descr: 'Spacious apartment in the city center',
-  rating: 4,
-  price: 2153,
-};
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
 
-const keys = Object.keys(apartment);
-const values = [];
+// const keys = Object.keys(apartment);
+// const values = [];
 
-for (const key of keys) {
-  values.push(apartment[key]);
-}
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
 
-console.log(keys);
-console.log(values);
+// console.log(keys);
+// console.log(values);
 
 // ======================= //
 // ======= task-04 ======= //
+
+// Виконай рефакторинг функції countProps(object), замінивши перебір ключів за допомогою циклу for…in на метод Object.keys() для отримання масиву властивостей
+// Функція має повернути кількість властивостей в об'єкті object
+
+// function countProps(object) {
+//   let propCount = 0;
+
+//   for (const key in object) {
+//     if (object.hasOwnProperty(key)) {
+//       propCount += 1;
+//     }
+//   }
+
+//   return propCount;
+// }
+
+function countProps(object) {
+  return Object.keys(object).length;
+}
+
+console.log(countProps({ name: 'Mango', age: 2 }));
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
+
 // ======================= //
 // ======= task-05 ======= //
 // ======================= //
