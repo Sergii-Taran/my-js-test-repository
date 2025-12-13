e19256d2a6b033dee9f065ea9f728a845288f7f7
Sergii Taran <serg.taran1970@gmail.com>
2025-12-13 16:25:26 +0200

    add solution homework-03 from module 04

--- js/task-4.js
+++ js/task-4.js
@@ -401,45 +401,87 @@
 // Функція очікує один параметр: days — масив об’єктів
 // Кожен об’єкт описує день тижня та кількість калорій calories, спожитих спортсменом, у цей день
 
-function calcAverageCalories(days) {
-  if (days.length === 0) {
-    return 0;
-  }
+// function calcAverageCalories(days) {
+//   if (days.length === 0) {
+//     return 0;
+//   }
 
-  let totalCalories = 0;
+//   let totalCalories = 0;
 
-  for (const day of days) {
-    totalCalories += day.calories;
-  }
-  return totalCalories / days.length;
-}
+//   for (const day of days) {
+//     totalCalories += day.calories;
+//   }
+//   return totalCalories / days.length;
+// }
 
-console.log(
-  calcAverageCalories([
-    { day: 'monday', calories: 3010 },
-    { day: 'tuesday', calories: 3200 },
-    { day: 'wednesday', calories: 3120 },
-    { day: 'thursday', calories: 2900 },
-    { day: 'friday', calories: 3450 },
-    { day: 'saturday', calories: 3280 },
-    { day: 'sunday', calories: 3300 },
-  ])
-); // 3180
+// console.log(
+//   calcAverageCalories([
+//     { day: 'monday', calories: 3010 },
+//     { day: 'tuesday', calories: 3200 },
+//     { day: 'wednesday', calories: 3120 },
+//     { day: 'thursday', calories: 2900 },
+//     { day: 'friday', calories: 3450 },
+//     { day: 'saturday', calories: 3280 },
+//     { day: 'sunday', calories: 3300 },
+//   ])
+// ); // 3180
 
-console.log(
-  calcAverageCalories([
-    { day: 'monday', calories: 2040 },
-    { day: 'tuesday', calories: 2270 },
-    { day: 'wednesday', calories: 2420 },
-    { day: 'thursday', calories: 1900 },
-    { day: 'friday', calories: 2370 },
-    { day: 'saturday', calories: 2280 },
-    { day: 'sunday', calories: 2610 },
-  ])
-); // 2270
+// console.log(
+//   calcAverageCalories([
+//     { day: 'monday', calories: 2040 },
+//     { day: 'tuesday', calories: 2270 },
+//     { day: 'wednesday', calories: 2420 },
+//     { day: 'thursday', calories: 1900 },
+//     { day: 'friday', calories: 2370 },
+//     { day: 'saturday', calories: 2280 },
+//     { day: 'sunday', calories: 2610 },
+//   ])
+// ); // 2270
 
-console.log(calcAverageCalories([])); // 0
+// console.log(calcAverageCalories([])); // 0
 
 // ======================= //
 // ======== hw-03 ======== //
+
+// є наступна задача:
+// Об’єкт profile описує профіль користувача на ігровій платформі
+// У його властивостях зберігається ім’я профілю username та кількість активних годин playTime, проведених у грі
+
+// const profile = {
+//    username: "Jacob",
+//  playTime: 300,
+// };
+
+// Доповни об’єкт profile методами для роботи з його властивостями
+// Метод changeUsername(newName) повинен приймати рядок (нове ім’я) в параметр newName та змінювати значення властивості username на нове. Нічого не повертає
+// Метод updatePlayTime(hours) повинен приймати число (кількість годин) у параметр hours та збільшити на нього значення властивості playTime. Нічого не повертає
+// Метод getInfo() має повертати рядок формату <Username> has <amount> active hours!, де <Username> — це ім’я профілю, а <amount> — кількість ігрових годин
+
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    if (typeof hours !== 'number') return;
+
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo());
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo());
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
+
 // ======================= //
