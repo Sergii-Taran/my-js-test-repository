e4ec66b54b77ad8bbdb88eefbf5dd6a5248c1a85
Sergii Taran <serg.taran1970@gmail.com>
2025-12-14 21:25:42 +0200

    add solution task-01 from module 05

--- js/task-5.js
+++ js/task-5.js
@@ -1,175 +1,43 @@
-const getUserNames = (users) => users.map((user) => user.name);
+// ======= task-01 ======= //
 
-console.log(
-  getUserNames([
-    {
-      name: 'Moore Hensley',
-      email: 'moorehensley@indexia.com',
-      balance: 2811,
-    },
-    {
-      name: 'Sharlene Bush',
-      email: 'sharlenebush@tubesys.com',
-      balance: 3821,
-    },
-    {
-      name: 'Ross Vazquez',
-      email: 'rossvazquez@xinware.com',
-      balance: 3793,
-    },
-    {
-      name: 'Elma Head',
-      email: 'elmahead@omatom.com',
-      balance: 2278,
-    },
-    {
-      name: 'Carey Barr',
-      email: 'careybarr@nurali.com',
-      balance: 3951,
-    },
-    {
-      name: 'Blackburn Dotson',
-      email: 'blackburndotson@furnigeer.com',
-      balance: 1498,
-    },
-    {
-      name: 'Sheree Anthony',
-      email: 'shereeanthony@kog.com',
-      balance: 2764,
-    },
-  ])
-);
+// Функція makeMessage приймає один параметр ім'я піци, що доставляється, pizzaName та повертає рядок з повідомленням клієнту
+// Доповни функцію makeMessage таким чином, щоб вона очікувала другим параметром (параметр callback) колбек-функцію і повертала результат її виклику
+// Функції deliverPizza або makePizza будуть передаватися як колбек для makeMessage і очікувати аргументом ім'я готової піци, що доставляється
 
-// ==================================
+function deliverPizza() {
+  return `Delivering ${pizzaName} pizza.`;
+}
 
-const allUsers = [
-  {
-    name: 'Moore Hensley',
-    friends: ['Sharron Pace'],
-  },
-  {
-    name: 'Sharlene Bush',
-    friends: ['Briana Decker', 'Sharron Pace'],
-  },
-  {
-    name: 'Ross Vazquez',
-    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-  },
-  {
-    name: 'Elma Head',
-    friends: ['Goldie Gentry', 'Aisha Tran'],
-  },
-  {
-    name: 'Carey Barr',
-    friends: ['Jordan Sampson', 'Eddie Strong'],
-  },
-  {
-    name: 'Blackburn Dotson',
-    friends: ['Jacklyn Lucas', 'Linda Chapman'],
-  },
-  {
-    name: 'Sheree Anthony',
-    friends: ['Goldie Gentry', 'Briana Decker'],
-  },
-];
+function makePizza() {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
 
-const getUsersWithFriend = (users, friendName) =>
-  users.filter((user) => user.friends.includes(friendName));
+function makeMessage(pizzaName, callback) {
+  return callback(pizzaName);
+}
 
-console.log(getUsersWithFriend(allUsers, 'Briana Decker'));
-console.log(getUsersWithFriend(allUsers, 'Goldie Gentry'));
-console.log(getUsersWithFriend(allUsers, 'Adrian Cross'));
+console.log(makeMessage('Royal Grand', makePizza));
+// "Pizza Royal Grand is being prepared, please wait..."
 
-// =====================
+console.log(makeMessage('Ultracheese', deliverPizza));
+// "Delivering Ultracheese pizza."
 
-const sortByDescendingFriendCount = (users) =>
-  users.toSorted((a, b) => b.friends.length - a.friends.length);
-
-console.log(
-  sortByDescendingFriendCount([
-    {
-      name: 'Moore Hensley',
-      friends: ['Sharron Pace'],
-      gender: 'male',
-    },
-    {
-      name: 'Sharlene Bush',
-      friends: ['Briana Decker', 'Sharron Pace'],
-      gender: 'female',
-    },
-    {
-      name: 'Ross Vazquez',
-      friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-      gender: 'male',
-    },
-    {
-      name: 'Elma Head',
-      friends: ['Goldie Gentry', 'Aisha Tran'],
-      gender: 'female',
-    },
-    {
-      name: 'Carey Barr',
-      friends: ['Jordan Sampson', 'Eddie Strong'],
-      gender: 'male',
-    },
-    {
-      name: 'Blackburn Dotson',
-      friends: ['Jacklyn Lucas', 'Linda Chapman'],
-      gender: 'male',
-    },
-    {
-      name: 'Sheree Anthony',
-      friends: ['Goldie Gentry', 'Briana Decker'],
-      gender: 'female',
-    },
-  ])
-);
-
-// ===========================
-
-const clients = [
-  {
-    name: 'Moore Hensley',
-    gender: 'male',
-    balance: 2811,
-  },
-  {
-    name: 'Sharlene Bush',
-    gender: 'female',
-    balance: 3821,
-  },
-  {
-    name: 'Ross Vazquez',
-    gender: 'male',
-    balance: 3793,
-  },
-  {
-    name: 'Elma Head',
-    gender: 'female',
-    balance: 2278,
-  },
-  {
-    name: 'Carey Barr',
-    gender: 'male',
-    balance: 3951,
-  },
-  {
-    name: 'Blackburn Dotson',
-    gender: 'male',
-    balance: 1498,
-  },
-  {
-    name: 'Sheree Anthony',
-    gender: 'female',
-    balance: 2764,
-  },
-];
-
-const getTotalBalanceByGender = (users, gender) =>
-  users
-    .filter((user) => user.gender === gender)
-    .reduce((total, user) => total + user.balance, 0);
-
-console.log(getTotalBalanceByGender(clients, 'male')); // 12053
-
-console.log(getTotalBalanceByGender(clients, 'female')); // 8863
+// ======================= //
+// ======= task-02 ======= //
+// ======================= //
+// ======= task-03 ======= //
+// ======================= //
+// ======= task-04 ======= //
+// ======================= //
+// ======= task-05 ======= //
+// ======================= //
+// ======= task-06 ======= //
+// ======================= //
+// ======= task-07 ======= //
+// ======================= //
+// ======= task-08 ======= //
+// ======================= //
+// ======= task-09 ======= //
+// ======================= //
+// ======= task-10 ======= //
+// ======================= //
