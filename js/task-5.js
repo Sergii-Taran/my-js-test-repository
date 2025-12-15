29df11c30c91459b12ae613664b33fbaae550c3d
Sergii Taran <serg.taran1970@gmail.com>
2025-12-15 07:42:27 +0200

    add solution task-03 from module 05

--- js/task-5.js
+++ js/task-5.js
@@ -27,25 +27,43 @@
 // Функція повинна помножити числа між собою та передати результат дії до колбек-функції
 // Колбек-функція повинна, у разі, якщо, це число є парним - помножити це число на себе, якщо ні - взяти квадратний корінь з цього числа, та вивести результат у консоль лог
 
-function processNumbers(a, b, callback) {
-  const result = a * b;
-  callback(result);
-}
+// function processNumbers(a, b, callback) {
+//   const result = a * b;
+//   callback(result);
+// }
 
-function handleResult(number) {
-  if (number % 2 === 0) {
-    console.log((number *= number));
-  } else {
-    console.log(Math.sqrt(number));
-  }
-}
+// function handleResult(number) {
+//   if (number % 2 === 0) {
+//     console.log((number *= number));
+//   } else {
+//     console.log(Math.sqrt(number));
+//   }
+// }
 
-processNumbers(4, 2, handleResult);
-processNumbers(3, 3, handleResult);
-processNumbers(3, 2, handleResult);
+// processNumbers(4, 2, handleResult);
+// processNumbers(3, 3, handleResult);
+// processNumbers(3, 2, handleResult);
 
 // ======================= //
 // ======= task-03 ======= //
+
+// Функція calculateTotalPrice(orderedItems) приймає один параметр orderedItems - масив чисел, і розраховує загальну суму його елементів, яка зберігається у змінній totalPrice і повертається як результат роботи функції
+// Доповни виклик метода forEach, передавши йому колбек-функцію, яка на кожній ітерації додає до totalPrice значення поточного елемента масива orderedItems
+
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+
+  orderedItems.forEach(function (item) {
+    totalPrice += item;
+  });
+
+  return totalPrice;
+}
+
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291]));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
 // ======================= //
 // ======= task-04 ======= //
 // ======================= //
