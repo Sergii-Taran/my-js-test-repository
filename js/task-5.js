e10fc81a000a52055e734f7201b8907b0f749130
Sergii Taran <serg.taran1970@gmail.com>
2025-12-15 06:39:31 +0200

    add solution task-02 from module 05

--- js/task-5.js
+++ js/task-5.js
@@ -4,26 +4,46 @@
 // Доповни функцію makeMessage таким чином, щоб вона очікувала другим параметром (параметр callback) колбек-функцію і повертала результат її виклику
 // Функції deliverPizza або makePizza будуть передаватися як колбек для makeMessage і очікувати аргументом ім'я готової піци, що доставляється
 
-function deliverPizza() {
-  return `Delivering ${pizzaName} pizza.`;
-}
+// function deliverPizza() {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
 
-function makePizza() {
-  return `Pizza ${pizzaName} is being prepared, please wait...`;
-}
+// function makePizza() {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
 
-function makeMessage(pizzaName, callback) {
-  return callback(pizzaName);
-}
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
 
-console.log(makeMessage('Royal Grand', makePizza));
-// "Pizza Royal Grand is being prepared, please wait..."
+// console.log(makeMessage('Royal Grand', makePizza));
 
-console.log(makeMessage('Ultracheese', deliverPizza));
-// "Delivering Ultracheese pizza."
+// console.log(makeMessage('Ultracheese', deliverPizza));
 
 // ======================= //
 // ======= task-02 ======= //
+
+// Напишіть функцію, яка приймає два числа і колбек-функцію
+// Функція повинна помножити числа між собою та передати результат дії до колбек-функції
+// Колбек-функція повинна, у разі, якщо, це число є парним - помножити це число на себе, якщо ні - взяти квадратний корінь з цього числа, та вивести результат у консоль лог
+
+function processNumbers(a, b, callback) {
+  const result = a * b;
+  callback(result);
+}
+
+function handleResult(number) {
+  if (number % 2 === 0) {
+    console.log((number *= number));
+  } else {
+    console.log(Math.sqrt(number));
+  }
+}
+
+processNumbers(4, 2, handleResult);
+processNumbers(3, 3, handleResult);
+processNumbers(3, 2, handleResult);
+
 // ======================= //
 // ======= task-03 ======= //
 // ======================= //
