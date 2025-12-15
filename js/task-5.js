4a5fb57fcaf21d943437a960782529e62a2e87f0
Sergii Taran <serg.taran1970@gmail.com>
2025-12-15 12:48:51 +0200

    add solution task-06 from module 05

--- js/task-5.js
+++ js/task-5.js
@@ -108,22 +108,54 @@
 //   return totalPrice;
 // }
 
-const calculateTotalPrice = (orderedItems) => {
-  let totalPrice = 0;
+// const calculateTotalPrice = (orderedItems) => {
+//   let totalPrice = 0;
 
-  orderedItems.forEach((item) => {
-    totalPrice += item;
-  });
+//   orderedItems.forEach((item) => {
+//     totalPrice += item;
+//   });
 
-  return totalPrice;
-};
+//   return totalPrice;
+// };
 
-console.log(calculateTotalPrice([12, 85, 37, 4]));
-console.log(calculateTotalPrice([164, 48, 291]));
-console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
 
 // ======================= //
 // ======= task-06 ======= //
+
+// Заміни оголошення функції filterArray() і колбек для методу forEach() на стрілочні функції
+
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+
+//   numbers.forEach(function (number) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+//   return filteredNumbers;
+// }
+
+const filterArray = (numbers, value) => {
+  const filteredNumbers = [];
+
+  numbers.forEach((number) => {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  });
+  return filteredNumbers;
+};
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
+
 // ======================= //
 // ======= task-07 ======= //
 // ======================= //
