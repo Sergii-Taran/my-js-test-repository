a3face661e8c108562b79a07c5a4e892ed586f2c
Sergii Taran <serg.taran1970@gmail.com>
2025-12-17 16:48:47 +0200

    add solution homework-04 from module 05

--- js/task-5.js
+++ js/task-5.js
@@ -456,49 +456,101 @@
 // Напиши стрілочну функцію sortByDescendingFriendCount(users) , яка прийматиме один параметр users — масив об’єктів користувачів
 // Функція має повертати масив усіх користувачів, відсортованих за спаданням кількостій їх друзів (властивість friends)
 
-const sortByDescendingFriendCount = (users) =>
-  users.toSorted((a, b) => b.friends.length - a.friends.length);
+// const sortByDescendingFriendCount = (users) =>
+//   users.toSorted((a, b) => b.friends.length - a.friends.length);
 
-console.log(
-  sortByDescendingFriendCount([
-    {
-      name: 'Moore Hensley',
-      friends: ['Sharron Pace'],
-      gender: 'male',
-    },
-    {
-      name: 'Sharlene Bush',
-      friends: ['Briana Decker', 'Sharron Pace'],
-      gender: 'female',
-    },
-    {
-      name: 'Ross Vazquez',
-      friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-      gender: 'male',
-    },
-    {
-      name: 'Elma Head',
-      friends: ['Goldie Gentry', 'Aisha Tran'],
-      gender: 'female',
-    },
-    {
-      name: 'Carey Barr',
-      friends: ['Jordan Sampson', 'Eddie Strong'],
-      gender: 'male',
-    },
-    {
-      name: 'Blackburn Dotson',
-      friends: ['Jacklyn Lucas', 'Linda Chapman'],
-      gender: 'male',
-    },
-    {
-      name: 'Sheree Anthony',
-      friends: ['Goldie Gentry', 'Briana Decker'],
-      gender: 'female',
-    },
-  ])
-);
+// console.log(
+//   sortByDescendingFriendCount([
+//     {
+//       name: 'Moore Hensley',
+//       friends: ['Sharron Pace'],
+//       gender: 'male',
+//     },
+//     {
+//       name: 'Sharlene Bush',
+//       friends: ['Briana Decker', 'Sharron Pace'],
+//       gender: 'female',
+//     },
+//     {
+//       name: 'Ross Vazquez',
+//       friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//       gender: 'male',
+//     },
+//     {
+//       name: 'Elma Head',
+//       friends: ['Goldie Gentry', 'Aisha Tran'],
+//       gender: 'female',
+//     },
+//     {
+//       name: 'Carey Barr',
+//       friends: ['Jordan Sampson', 'Eddie Strong'],
+//       gender: 'male',
+//     },
+//     {
+//       name: 'Blackburn Dotson',
+//       friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//       gender: 'male',
+//     },
+//     {
+//       name: 'Sheree Anthony',
+//       friends: ['Goldie Gentry', 'Briana Decker'],
+//       gender: 'female',
+//     },
+//   ])
+// );
 
 // =========================== //
 // ======= homework-04 ======= //
+
+// Напиши стрілочну функцію getTotalBalanceByGender(users, gender), яка прийматиме два параметра:
+// перший параметр users — масив об’єктів користувачів,
+// другий параметр gender — рядок, що зберігає стать.
+// Функція має використовувати ланцюжок виклику методів та повертати загальний баланс користувачів (властивість balance), стать яких (властивість gender) збігається зі значенням параметра gender
+
+const getTotalBalanceByGender = (users, gender) =>
+  users
+    .filter((user) => user.gender === gender)
+    .reduce((total, user) => total + user.balance, 0);
+
+const clients = [
+  {
+    name: 'Moore Hensley',
+    gender: 'male',
+    balance: 2811,
+  },
+  {
+    name: 'Sharlene Bush',
+    gender: 'female',
+    balance: 3821,
+  },
+  {
+    name: 'Ross Vazquez',
+    gender: 'male',
+    balance: 3793,
+  },
+  {
+    name: 'Elma Head',
+    gender: 'female',
+    balance: 2278,
+  },
+  {
+    name: 'Carey Barr',
+    gender: 'male',
+    balance: 3951,
+  },
+  {
+    name: 'Blackburn Dotson',
+    gender: 'male',
+    balance: 1498,
+  },
+  {
+    name: 'Sheree Anthony',
+    gender: 'female',
+    balance: 2764,
+  },
+];
+
+console.log(getTotalBalanceByGender(clients, 'male'));
+console.log(getTotalBalanceByGender(clients, 'female'));
+
 // =========================== //
