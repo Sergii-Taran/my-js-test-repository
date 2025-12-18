bdab58a060852ffd01941fb510c8ea4de90d7b49
Sergii Taran <serg.taran1970@gmail.com>
2025-12-18 15:43:19 +0200

    add solution task-02 from module 06

--- js/task-6.js
+++ js/task-6.js
@@ -2,29 +2,46 @@
 
 // Виконай рефакторинг методів об'єкта pizzaPalace, розставивши відсутні this в місцях звернення до властивостей і методів об'єкта
 
-const pizzaPalace = {
-  pizzas: ['Supercheese', 'Smoked', 'Four meats'],
-  checkPizza(pizzaName) {
-    return this.pizzas.includes(pizzaName);
-  },
-  order(pizzaName) {
-    const isPizzaAvailable = this.checkPizza(pizzaName);
+// const pizzaPalace = {
+//   pizzas: ['Supercheese', 'Smoked', 'Four meats'],
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
 
-    if (!isPizzaAvailable) {
-      return `Sorry, there is no pizza named «${pizzaName}»`;
-    }
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named «${pizzaName}»`;
+//     }
 
-    return `Order accepted, preparing «${pizzaName}» pizza`;
-  },
-};
+//     return `Order accepted, preparing «${pizzaName}» pizza`;
+//   },
+// };
 
-console.log(pizzaPalace.order('Smoked'));
-console.log(pizzaPalace.order('Four meats'));
-console.log(pizzaPalace.order('Big Mike'));
-console.log(pizzaPalace.order('Viennese'));
+// console.log(pizzaPalace.order('Smoked'));
+// console.log(pizzaPalace.order('Four meats'));
+// console.log(pizzaPalace.order('Big Mike'));
+// console.log(pizzaPalace.order('Viennese'));
 
 // ======================= //
 // ======= task-02 ======= //
+
+// Зміни код таким чином, щоб об'єкт parent став прототипом для об'єкта у змінній сhild
+
+const parent = {
+  name: 'Stacey',
+  surname: 'Moore',
+  age: 54,
+  heritage: 'Irish',
+};
+
+const child = Object.create(parent);
+child.name = 'Jason';
+child.age = 27;
+
+console.log(parent);
+console.log(child);
+
 // ======================= //
 // ======= task-03 ======= //
 // ======================= //
