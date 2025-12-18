8d1258a77da94e6fa7c0fd6c85830345d4633044
Sergii Taran <serg.taran1970@gmail.com>
2025-12-18 06:27:01 +0200

    add solution task-01 from module 06

--- js/task-6.js
+++ js/task-6.js
@@ -1,101 +1,44 @@
-const customer = {
-  username: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['Burger', 'Pizza', 'Salad'],
+// ======= task-01 ======= //
 
-  getBalance() {
-    return this.balance;
+// Виконай рефакторинг методів об'єкта pizzaPalace, розставивши відсутні this в місцях звернення до властивостей і методів об'єкта
+
+const pizzaPalace = {
+  pizzas: ['Supercheese', 'Smoked', 'Four meats'],
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
   },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      return `Sorry, there is no pizza named «${pizzaName}»`;
+    }
+
+    return `Order accepted, preparing «${pizzaName}» pizza`;
   },
 };
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount());
-customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance());
-console.log(customer.getOrders());
+console.log(pizzaPalace.order('Smoked'));
+console.log(pizzaPalace.order('Four meats'));
+console.log(pizzaPalace.order('Big Mike'));
+console.log(pizzaPalace.order('Viennese'));
 
-// ==============================
-
-class Storage {
-  #items;
-
-  constructor(items) {
-    this.#items = items;
-  }
-
-  getItems() {
-    return this.#items;
-  }
-
-  addItem(newItem) {
-    this.#items.push(newItem);
-  }
-
-  removeItem(itemToRemove) {
-    this.#items = this.#items.filter((item) => item !== itemToRemove);
-  }
-}
-
-const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
-console.log(storage.getItems());
-
-storage.addItem('Droid');
-console.log(storage.getItems());
-
-storage.removeItem('Prolonger');
-console.log(storage.getItems());
-
-storage.removeItem('Scaner');
-console.log(storage.getItems());
-
-// ===============================
-
-class StringBuilder {
-  #value;
-
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
-
-const builder = new StringBuilder('.');
-console.log(builder.getValue());
-
-builder.padStart('^');
-console.log(builder.getValue());
-
-builder.padEnd('^');
-console.log(builder.getValue());
-
-builder.padBoth('=');
-console.log(builder.getValue());
+// ======================= //
+// ======= task-02 ======= //
+// ======================= //
+// ======= task-03 ======= //
+// ======================= //
+// ======= task-04 ======= //
+// ======================= //
+// ======= task-05 ======= //
+// ======================= //
+// ======= task-06 ======= //
+// ======================= //
+// ======= task-07 ======= //
+// ======================= //
+// ======= task-08 ======= //
+// ======================= //
+// ======= task-09 ======= //
+// ======================= //
+// ======= task-10 ======= //
+// ======================= //
