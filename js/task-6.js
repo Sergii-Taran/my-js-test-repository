aaef72e17204be92d31969b1082d34bfa3fb85d8
Sergii Taran <serg.taran1970@gmail.com>
2025-12-19 14:41:50 +0200

    add solution task-10 from module 06

--- js/task-6.js
+++ js/task-6.js
@@ -241,35 +241,75 @@
 // Якщо воно більше за maxPrice, сеттер нічого не робить, а якщо менше або дорівнює, то перезаписує ціну автомобіля
 // Під оголошенням класу ми додали ініціалізації екземплярів і виклики методів, щоб показати, як будуть використовуватися геттери і сеттери price
 
-class Car {
-  static maxPrice = 50000;
+// class Car {
+//   static maxPrice = 50000;
 
-  #price;
+//   #price;
 
-  constructor(params) {
-    this.#price = params.price;
-  }
+//   constructor(params) {
+//     this.#price = params.price;
+//   }
 
-  get price() {
-    return this.#price;
-  }
+//   get price() {
+//     return this.#price;
+//   }
 
-  set price(newPrice) {
-    if (newPrice <= Car.maxPrice) {
-      this.#price = newPrice;
-    }
-  }
-}
+//   set price(newPrice) {
+//     if (newPrice <= Car.maxPrice) {
+//       this.#price = newPrice;
+//     }
+//   }
+// }
 
-const audi = new Car({ price: 35000 });
-console.log(audi.price); // 35000
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price);
 
-audi.price = 49000;
-console.log(audi.price); // 49000
+// audi.price = 49000;
+// console.log(audi.price);
 
-audi.price = 51000;
-console.log(audi.price); // 49000
+// audi.price = 51000;
+// console.log(audi.price);
 
 // ======================= //
 // ======= task-10 ======= //
+
+// Додай класу Car публічний статичний метод checkPrice(price), що приймає ціну автомобіля
+// Метод повинен порівняти значення параметра price і приватної статичної властивості maxPrice
+// Якщо ціна автомобіля перевищує максимальну, метод повинен повернути рядок "Error! Price exceeds the maximum"
+// В іншому випадку метод повинен повернути рядок "Success! Price is within acceptable limits"
+// Під оголошенням класу додано ініціалізацію екземпляра і виклики методів, щоб показати, як буде використовуватися метод checkPrice(price)
+
+class Car {
+  static #maxPrice = 50000;
+
+  constructor(params) {
+    this.price = params.price;
+  }
+
+  static checkPrice(price) {
+    if (price > Car.#maxPrice) {
+      return 'Error! Price exceeds the maximum';
+    }
+    return 'Success! Price is within acceptable limits';
+  }
+}
+
+const audi = new Car({ price: 36000 });
+const bmw = new Car({ price: 64000 });
+
+console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+// ======================= //
+// ======= task-11 ======= //
+// ======================= //
+// ======= task-12 ======= //
+// ======================= //
+// ======= task-13 ======= //
+// ======================= //
+// ======= task-14 ======= //
+// ======================= //
+// ======= task-15 ======= //
+// ======================= //
+// ======= task-16 ======= //
 // ======================= //
