6f04b3696d6720d66b4754497de514bd2747712e
Sergii Taran <serg.taran1970@gmail.com>
2025-12-23 09:37:03 +0200

    add solution task-07 from blended 05-06

--- js/task-blended-05-06.js
+++ js/task-blended-05-06.js
@@ -88,14 +88,25 @@
 // Відсортуйте масив чисел [4, 2, 5, 1, 3]
 // у порядку зростання. Очікуваний результат: [1, 2, 3, 4, 5].
 
-const numbersArray = [4, 2, 5, 1, 3];
+// const numbersArray = [4, 2, 5, 1, 3];
 
-const sortedArray = numbersArray.toSorted((a, b) => a - b);
+// const sortedArray = numbersArray.toSorted((a, b) => a - b);
 
-console.log(sortedArray);
+// console.log(sortedArray);
 
 // ========================= //
 // ======== task-07 ======== //
+
+// Відсортуйте масив рядків ["banana", "orange", "apple", "pear"]
+//  у порядку алфавіту.
+// Очікуваний результат: ["apple", "banana", "orange", "pear"].
+
+const stringArray = ['banana', 'orange', 'apple', 'pear'];
+
+const sortedArray = stringArray.toSorted((a, b) => a.localeCompare(b));
+
+console.log(sortedArray);
+
 // ========================= //
 // ======== task-08 ======== //
 // ========================= //
